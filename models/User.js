dc08f1712cd665a68a2f29cdf04da0c66a3544dd
Nneif <nneifemakpunonu@gmail.com>
2023-07-25 15:45:30 -0400

    filled in some code in the server, connection, and index js files

--- models/User.js
+++ models/User.js
@@ -8,6 +8,37 @@ const userSchema = new Schema(
             required: true,
             //trimmed
         },
-        
+        email: {
+            type: String,
+            required: true,
+            //unique
+            // validate email address
+        },
+        thoughts: [
+            {
+                type: Schema.type.ObjectId,
+                ref: "Thought"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.type.ObjectId,
+                ref: "User"
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virutals: true,
+            getters: true,
+        },
     }
 );
+
+userSchema.virtual("friendCount").get(function(){
+    return this.friend.length
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
